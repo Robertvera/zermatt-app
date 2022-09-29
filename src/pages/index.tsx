83699dcc120f0e4b6eb201fbaf85a23b2cf5111a
Robert Vera <robertveraroige@gmail.com>
2022-09-29 16:14:35 +0200

    still mo fire

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home({ investments }: User) {
 }
 
 export const getServerSideProps: GetServerSideProps<User> = async (context) => {
-  console.log('FETCHING DATA... 🔥🔥')
+  console.log('FETCHING DATA... 🔥🔥🔥')
   
   return {
     props: FAKE_USER_DATA
